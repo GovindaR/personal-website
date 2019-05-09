19df05199964a151a8849b311b738b7426ad7142
Govinda <vegarana44@gmail.com>
2019-05-09 08:47:47 +0545

    Updates

---
+++ precache-manifest.1d0678bc9f443c10a7e0aecb31bc2242.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b209338267b40059264f0a7e8fc1c9f",
+    "url": "/GovindaR/personal-website/static/media/award.2b209338.png"
+  },
+  {
+    "revision": "98198c39ad4f07a4d3d8",
+    "url": "/GovindaR/personal-website/static/js/runtime~main.130aeceb.js"
+  },
+  {
+    "revision": "c208e679ed42f62399fd",
+    "url": "/GovindaR/personal-website/static/js/main.de471181.chunk.js"
+  },
+  {
+    "revision": "2bd3b61ef54a10baae06",
+    "url": "/GovindaR/personal-website/static/js/2.3689f7b0.chunk.js"
+  },
+  {
+    "revision": "c208e679ed42f62399fd",
+    "url": "/GovindaR/personal-website/static/css/main.8f91d7e7.chunk.css"
+  },
+  {
+    "revision": "801e1dd651ddce880f8693ff99644aae",
+    "url": "/GovindaR/personal-website/index.html"
+  }
+];
